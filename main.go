d9715e51f0ddefa3eac94a4853632cc351e82fe2
@ivelton2018 <ivelton2018@gmail.com>
2018-10-25 10:43:55 -0400

    new card

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
-	fmt.Println("Cards Game")
+	var card string = newCard()
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
